104708740c0cd8811fba7b67b2a59f936088eeb7
Brianna Schuyler <brianna.schuyler@gmail.com>
2017-01-15 11:01:07 -0800

    Add user loan elements to website

--- find_similar_loans.py
+++ find_similar_loans.py
@@ -12,48 +12,70 @@ def get_user_loan_elements(user):
     response = requests.get(url)
     lender = eval(response.content.replace('false', 'False').replace('true', 'True'))
 
-    user_loan_elements = set()
+    # Make sets of each of these important categories of the user's loans
+    countries, continents, sectors, tags, themes = set(), set(), set(), set(), set()
 
     for loan in range(len(lender['loans'])):
         if 'country' in lender['loans'][loan]['location']:
-            user_loan_elements.update([lender['loans'][loan]['location']['country']])
-            user_loan_elements.update([country_to_continent.get(lender['loans'][loan]['location']['country'])])
+            countries.update([lender['loans'][loan]['location']['country']])
+            continents.update([country_to_continent.get(lender['loans'][loan]['location']['country'])])
 
         if 'sector'in lender['loans'][loan]:
-            user_loan_elements.update([lender['loans'][loan]['sector']])
+            sectors.update([lender['loans'][loan]['sector']])
 
         if 'tags' in lender['loans'][loan]:
-            tags = [k['name'].strip('#') for k in lender['loans'][loan]['tags']]
-            user_loan_elements.update(tags)
+            loan_tags = [k['name'].strip('#') for k in lender['loans'][loan]['tags']]
+            tags.update(loan_tags)
 
         if 'themes' in lender['loans'][loan]:
-            themes = lender['loans'][loan]['themes']
-            user_loan_elements.update(themes)
+            loan_themes = lender['loans'][loan]['themes']
+            themes.update(loan_themes)
+
+        user_loan_elements = {'user_countries': countries,
+                              'user_continents': continents,
+                              'user_sectors': sectors,
+                              'user_tags': tags,
+                              'user_themes': themes}
 
     return user_loan_elements
 
+def get_user_loan_elements_set(user_loan_elements):
+    # Combine elements from each category into a single set to calculate similarity
+    user_loan_elements_set = set()
+    for el in user_loan_elements:
+        user_loan_elements_set.update(user_loan_elements[el])
+    return user_loan_elements_set
+
 def jaccard_distance(x, user_loan_elements):
-    intersection = len(set.intersection(x, user_loan_elements))
-    union = len(set.union(x, user_loan_elements))
+    user_loan_elements_set = get_user_loan_elements_set(user_loan_elements)
+    intersection = len(set.intersection(x, user_loan_elements_set))
+    union = len(set.union(x, user_loan_elements_set))
     if union > 0:
         return intersection/float(union)
     else:
         return 0
 
 def get_loans_to_display(loan_similarity, number_displayed):
+    # Return a list of tuples (loand_id, similarity_score) sorted by similarity
     number_displayed = 10
     if number_displayed > len(loan_similarity):
         number_displayed = len(loan_similarity)
 
     loans_to_display = []
     for i in range(number_displayed):
-        loans_to_display.append(loan_similarity.pop(max(loan_similarity)))
+        next_max_sim = max(loan_similarity)
+        loans_to_display.append((loan_similarity[next_max_sim], next_max_sim))
+        loan_similarity.pop(max(loan_similarity))
 
     return loans_to_display
 
+def get_percent(x):
+    return str('%s%%' % int(round(x * 100)))
+
 def get_loan_details_from_api(loans_to_display):
     url = 'http://api.kivaws.org/v1/loans/'
-    for loan_id in loans_to_display:
+    for loan_sim in loans_to_display:
+        loan_id = loan_sim[0]
         url += '%s,' % loan_id
     url = url[:-1] + '.json'
 
@@ -69,26 +91,42 @@ def get_loan_details_from_api(loans_to_display):
 
     # Pick the values of that dictionary to be displayed on the main page
     loan_details_to_display = []
-    for loan_id in loans_to_display:
-	loan_link = 'https://www.kiva.org/lend/%s' % loan_id
-	if loan_details[loan_id]['borrowers'][0]['pictured'] == True:
-	    img = loan_details[loan_id]['image']['id']
-	    loan_img = 'https://www.kiva.org/img/s400/%s.jpg' % img
-	else:
-	    loan_img = 'static/kiva.png'
-	country = loan_details[loan_id]['location']['country']
-	continent = country_to_continent.get(country, 'Unknown')
-	text = loan_details[loan_id]['description']['texts']['en']
+    for loan_sim in loans_to_display:
+        loan_id = loan_sim[0]
+
+        # Image and link to loan
+        loan_link = 'https://www.kiva.org/lend/%s' % loan_id
+        if loan_details[loan_id]['borrowers'][0]['pictured'] == True:
+            img = loan_details[loan_id]['image']['id']
+            loan_img = 'https://www.kiva.org/img/s400/%s.jpg' % img
+        else:
+            loan_img = 'static/kiva.png'
+
+        # Borrower details
+        borrower_name =  loan_details[loan_id]['borrowers'][0]['first_name']
+        if len(loan_details[loan_id]['borrowers']) > 1:
+            gender = ''
+        else:
+            gender = loan_details[loan_id]['borrowers'][0]['gender']
+
+        # Loan details
+        country = loan_details[loan_id]['location']['country']
+        continent = country_to_continent.get(country, 'Unknown')
+        sector = loan_details[loan_id]['sector']
+        text = loan_details[loan_id]['description']['texts']['en']
+
+        # Send details to website to display
         loan_details_to_display.append({'loan_id': loan_id,
-					'loan_link': loan_link,
-					'loan_img': loan_img,
-					'borrower_name': loan_details[loan_id]['borrowers'][0]['first_name'],
-					'gender': loan_details[loan_id]['borrowers'][0]['gender'],
+                                        'similarity': get_percent(loan_sim[1]),
+                                        'loan_link': loan_link,
+                                        'loan_img': loan_img,
+                                        'borrower_name': borrower_name,
+                                        'gender': gender,
                                         'country': country,
-					'continent': continent,
-                                        'sector': loan_details[loan_id]['sector'],
-					'text': text
-					})
+                                        'continent': continent,
+                                        'sector': sector,
+                                        'text': text
+                                        })
     return loan_details_to_display
 
 
@@ -101,10 +139,11 @@ def main(user, number_displayed):
 
     # Find the similarity of every loan with the user's loans
     loan_similarity = {jaccard_distance(v['elements'], user_loan_elements): k for k, v in loan_elements.iteritems()}
+
     # Pick the currently active loans with the highest similarity to display
     loans_to_display = get_loans_to_display(loan_similarity, number_displayed)
 
     # Find details on the loans to be displayed
     loan_details_to_display = get_loan_details_from_api(loans_to_display)
 
-    return loan_details_to_display
+    return user_loan_elements, loan_details_to_display
--- recommender/views.py
+++ recommender/views.py
@@ -14,14 +14,51 @@ def index():
                          title = "Kiva Loan Recommender",
                          user = user)
 
+def redirect_message():
+  return '''
+<b>This username does not exist in the Kiva database or is not public!</b> <br><br>
+
+To make your account public, go to <a href="https://www.kiva.org/settings/account" target="_blank">Kiva Account Settings</a>.<br>
+(Make sure you're signed in.)<br>
+At the bottom of the page, click "Make my page and loans public."<br>
+More details can be found <a href="http://pages.kiva.org/kivablog/2013/08/08/your-kiva-account-settings-just-got-that-much-simpler">here</a>.<br><br>
+
+Once you're done, return to the <a href="index">Kiva Loan Recommender</a> to find the loans you'll likely be interested in!
+'''
+
+def find_username_message():
+  return '''
+To find your username (and make your account public), go to <a href="https://www.kiva.org/settings/account" target="_blank">Kiva Account Settings</a>.<br>
+(Make sure you're signed in.)<br>
+At the bottom of the page, click "Make my page and loans public."<br>
+More details can be found <a href="http://pages.kiva.org/kivablog/2013/08/08/your-kiva-account-settings-just-got-that-much-simpler">here</a>.<br><br>
+
+Once you're done, return to the <a href="index">Kiva Loan Recommender</a> to find the loans you'll likely be interested in!
+'''
+
+@app.route('/find_username')
+def find_username():
+  return find_username_message()
+
+def set_to_string(input_set):
+  output = ''
+  for s in input_set:
+    output = output + s + ', '
+  return output[:-2]
 
 @app.route('/best_loans')
 def best_loans():
-  #user = 'brianna9306'
-
-  user_name = request.args.get('user_name')
-  print(user_name)
+  username = request.args.get('username')
   number_displayed = 10
-  best_loans= find_similar_loans.main(user_name, number_displayed)
-  # Display the most similar loans, from best to worst
-  return render_template("best_loans.html", best_loans=best_loans)
+  print('Username %s' % username)
+  try:
+    user_loans, best_loans = find_similar_loans.main(username, number_displayed)
+  except:
+    return redirect_message()
+
+  # If username is in the system, return elements of user's previous loans and a list of similar
+  # loans, from best to worst
+  for element in user_loans:
+      user_loans[element] = set_to_string(user_loans[element])
+
+  return render_template("best_loans.html", user_loans=user_loans, best_loans=best_loans)
