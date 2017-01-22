5cbc0b41d203f99768312e98ee39a7f9c3bad95d
Brianna Schuyler <brianna.schuyler@gmail.com>
2017-01-22 15:06:40 -0800

    Ignore usage.log

--- recommender/views.py
+++ recommender/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request
+import json
 import logging
 import pickle
 
 import find_similar_loans
 from recommender import app
+from utils import set_to_string
 
 logging.basicConfig(filename='usage.log', format='%(asctime)s %(message)s', level=logging.INFO)
 
@@ -31,22 +33,17 @@ Your username is listed in the "My Lender Profile" section.<br>
 
 At the bottom of the page, click "Make my page and loans public."<br><br>
 
-More details can be found <a href="http://pages.kiva.org/kivablog/2013/08/08/your-kiva-account-settings-just-got-that-much-simpler">
+More details can be found
+<a href="http://pages.kiva.org/kivablog/2013/08/08/your-kiva-account-settings-just-got-that-much-simpler">
 here</a>.<br><br>
 
-Once you're done, return to the <a href="index">Kiva Loan Recommender</a> to find the loans you'll likely be interested in!
+Once you're done, return to the <a href="index">Kiva Loan Recommender</a> to find the loans you'll
+likely be interested in!
 '''
 
 @app.route('/find_username')
 def find_username():
   return find_username_message()
-
-def set_to_string(input_set):
-  output = ''
-  for s in input_set:
-    if s is not None:
-      output = output + s + ', '
-  return output[:-2]
 
 @app.route('/best_loans')
 def best_loans():
@@ -63,4 +60,16 @@ def best_loans():
   for element in user_loans:
     user_loans[element] = set_to_string(user_loans[element])
 
-  return render_template("best_loans.html", user_loans=user_loans, best_loans=best_loans)
+  return render_template("best_loans.html", username=json.dumps(username), user_loans=user_loans, best_loans=best_loans)
+
+@app.route('/countries_pie')
+def countries_pie():
+    #username = str(request.args.get('username'))
+    countries_data = [
+    { 'name': 'Microsoft Internet Explorer', 'y': 6, 'color': 'blue' },
+    { 'name': 'Chrome', 'y': 10 , 'color': 'blue' },
+    { 'name': 'Firefox', 'y': 1 , 'color': 'blue' }
+    ]
+
+    return json.dumps([countries_data, 'Countries AYAYAY', username])
+
--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
 def eval_string(x):
-    if type(x) == str:
-         return eval(x)
-    else:
-        return []
+  if type(x) == str:
+    return eval(x)
+  else:
+    return []
 
 def get_date(x):
-    from datetime import datetime
-    return datetime.strptime(x, "%Y-%m-%dT%H:%M:%SZ").date()
+  from datetime import datetime
+  return datetime.strptime(x, "%Y-%m-%dT%H:%M:%SZ").date()
+
+def set_to_string(input_set):
+  # Takes a set as input and outputs a string with the elements of that set
+  output = ''
+  for s in input_set:
+    if s is not None:
+      output = output + s + ', '
+  return output[:-2]
