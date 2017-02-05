d2ebbc2f4a9e71e4a6ee6f1ef6f92d342d75c147
Ubuntu <ubuntu@ip-172-31-2-182.us-west-1.compute.internal>
2017-02-05 23:26:49 +0000

    Change port, fix max instance calculation for user loans

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 from recommender import app
-app.run(debug=True, host='0.0.0.0', port=5000)
+
+# Port has been changed from 5000 to 80 so the domain name (kivaloans4.me) doesn't need a :5000 on
+# the end.  However, root is required to run any port below 1024 so I have to use the command:
+# >> sudo /home/ubuntu/anaconda3/bin/python run.py
+# Since sudo automatically wants to call its own version of python, so I have to be direct and
+# specify it.
+#app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=80)
+
 #app.run(debug=True)
